5ae452500092881f2e291bd5855254e2dc4a3f08
Kauê Guerra <kaueguerra@gmail.com>
2022-07-17 21:22:19 -0300

    other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import './styles.css'
+import Logo from '../../assets/img/logo.svg'
+
+function Header(){
+    return (
+        <header>
+        <div className="dsmeta-logo-container">
+            <img src={Logo} alt="DSMeta" />
+            <h1>DSMeta</h1>
+            <p>
+              Desenvolvido por
+              <a href="https://www.instagram.com/kaueguerra/"> Kauê Guerra</a>
+            </p>
+        </div>
+    </header>
+    )
+}
+
+export default Header;
